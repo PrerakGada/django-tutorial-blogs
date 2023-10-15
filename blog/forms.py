64fbca8d21c0ea7d1e2aaa3f00ee4eb0058704f2
Prerak Gada <prerakgada07@gmail.com>
2023-10-16 03:48:23 +0530

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+# forms.py
+
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
