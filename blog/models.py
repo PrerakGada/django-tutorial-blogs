c240b59e72a6587f5776b234b1cc92348a629b3a
bamjam101 <herejamil123@gmail.com>
2023-10-16 04:19:45 +0530

    cleanup

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from tinymce.models import HTMLField
-from django.contrib.auth.models import User
-
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = HTMLField()
     date = models.DateField(auto_now_add=True)
-    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,53 +2,28 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from blog.forms import ArticleForm
+from .forms import ArticleForm
 from .models import Article
-
 
 class Index(ListView):
     model = Article
-    queryset = Article.objects.all().order_by('-date')
     template_name = 'blog/index.html'
-    paginate_by = 1
-
-
-class Featured(ListView):
-    model = Article
-    queryset = Article.objects.filter(featured=True).order_by('-date')
-    template_name = 'blog/featured.html'
-    paginate_by = 1
-
+    context_object_name = 'articles'
+    ordering = ['-date']
 
 class DetailArticleView(DetailView):
     model = Article
     template_name = 'blog/blog_post.html'
+    context_object_name = 'article'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(DetailArticleView, self).get_context_data(
-            *args, **kwargs)
-        context['liked_by_user'] = False
-        article = Article.objects.get(id=self.kwargs.get('pk'))
-        if article.likes.filter(pk=self.request.user.id).exists():
-            context['liked_by_user'] = True
-        return context
-
-
-
-class DeleteArticleView(UserPassesTestMixin, DeleteView):
+class DeleteArticleView(DeleteView):
     model = Article
     template_name = 'blog/blog_delete.html'
     success_url = reverse_lazy('index')
 
-    def test_func(self):
-        article = Article.objects.get(id=self.kwargs.get('pk'))
-        return self.request.user.id == article.author.id
-
-
 class CreateBlogView(View):
-    template_name = 'blog/create_blog.html'  # Create a template for the form
+    template_name = 'blog/create_blog.html'
 
     def get(self, request):
         form = ArticleForm()
@@ -57,7 +32,6 @@ class CreateBlogView(View):
     def post(self, request):
         form = ArticleForm(request.POST)
         if form.is_valid():
-            new_article = form.save(commit=False)
-            new_article.save()
+            form.save()
             return redirect('index')
         return render(request, self.template_name, {'form': form})
